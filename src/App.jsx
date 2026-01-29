b098b28d1f2673e222ce9ae58dce5a40a55a0433
Kuplinov7788 <emirhan.7788.8899@gmail.com>
2026-01-29 18:25:37 +0500

    mainga push qlinmid eshaklar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <header></header>
+      <main>
+        <section></section>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default App;
